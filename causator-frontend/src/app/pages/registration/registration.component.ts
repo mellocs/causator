baea8c679296cec8ccd5a431e623ca8f81d1a721
Karigo333 <igorokkarpovich99@gmail.com>
2024-02-22 10:43:57 +0200

    auth

--- causator-frontend/src/app/pages/registration/registration.component.ts
+++ causator-frontend/src/app/pages/registration/registration.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule, NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators, } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators, } from '@angular/forms';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { threadId } from 'worker_threads';
 import { LoginComponent } from '../login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-registration',
@@ -17,7 +19,9 @@ import { LoginComponent } from '../login/login.component';
     RouterLink, 
     RouterModule, 
     RouterOutlet,
-    LoginComponent],
+    LoginComponent,
+    FormsModule
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
@@ -27,10 +31,9 @@ export class RegistrationComponent {
 
   showPassword() {
     this.hidePassword = !this.hidePassword;
-    console.log(this.hidePassword)
   }
 
-  constructor () {
+  constructor (private readonly authService: AuthService) {
     this.userData = new FormGroup({
       email: new FormControl('', [
         Validators.required,
@@ -40,16 +43,22 @@ export class RegistrationComponent {
         Validators.required,
         Validators.minLength(8),
         Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/)
+      ]),
+      password_confirmation: new FormControl('', [
+        Validators.required,
+        
       ])
     }) 
   }
 
+
+  
   submitRegistration() {
-    if (this.userData.valid) {
+    if (this.userData.valid && (this.userData.value.password === this.userData.value.password_confirmation)) {
+      this.authService.signUp(this.userData.value)
       console.log(this.userData.value);
     } else {
-      console.log("invalid data");
-    }
-    
+      console.log("invalid user data");
+    } 
   }
 }
--- causator-frontend/src/app/services/auth.service.ts
+++ causator-frontend/src/app/services/auth.service.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
+import { IAuthUser } from "../types/user.interface";
+import { API_URL } from "../constants/constants";
+import { catchError } from "rxjs";
 
 @Injectable({
-    providedIn: "root",
+    providedIn: 'root',
 })
 
 export class AuthService {
@@ -14,4 +17,13 @@ export class AuthService {
         
     }
 
+    signUp(userData: IAuthUser) {
+        return this.http.post(`${API_URL}/user`, userData)
+        .pipe(
+            catchError(err => {
+                throw new Error(err.message)
+            })
+        )
+        .subscribe();
+    }
 }
