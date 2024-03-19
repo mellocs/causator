d8c65b8d140fb4a61317bb3b0432e87c9618d337
Karigo333 <igorokkarpovich99@gmail.com>
2024-03-19 17:03:32 +0200

    fix

--- causator-frontend/src/app/components/notifications/notifications.component.ts
+++ causator-frontend/src/app/components/notifications/notifications.component.ts
@@ -1,9 +1,28 @@
+import { NgFor, NgIf, CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { ContactsComponent } from '../contacts/contacts.component';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    RouterLink,
+    RouterLinkActive,
+    DashboardComponent,
+    NgFor,
+    ContactsComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    CommonModule, 
+    HttpClientModule, 
+    RouterOutlet
+  ],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss'
 })
